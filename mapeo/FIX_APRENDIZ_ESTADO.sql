eaca75aded89399d0c3e8f6ce8443299a54a4ed7
victor manuel rodriguez arenilla <rodriguezkeiner@gmail.com>
2025-12-03 11:54:11 -0500

    Remove FIX_APRENDIZ_ESTADO.sql

--- mapeo/FIX_APRENDIZ_ESTADO.sql
+++
@@ -1,5 +0,0 @@
--- Agregar columna 'estado' a la tabla 'aprendiz'
-ALTER TABLE aprendiz ADD COLUMN estado VARCHAR(20) NOT NULL DEFAULT 'ACTIVO';
-
--- Verificar que la columna se agregó correctamente
-DESCRIBE aprendiz;

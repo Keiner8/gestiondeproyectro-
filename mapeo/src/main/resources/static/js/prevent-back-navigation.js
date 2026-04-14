c1c86f3f4d35864b2bd2909c78a690686bbc85de
victor manuel rodriguez arenilla <rodriguezkeiner@gmail.com>
2026-04-14 12:56:36 -0500

    Elimina proyecto Java mapeo

--- mapeo/src/main/resources/static/js/prevent-back-navigation.js
+++
@@ -1,8 +0,0 @@
-// ============================================================
-// PREVENCIÓN DE NAVEGACIÓN HACIA ATRÁS - DESHABILITADO
-// ============================================================
-
-console.log('Script prevent-back-navigation.js cargado pero deshabilitado');
-
-// El bloqueo de navegación estaba causando problemas
-// Déjalo habilitado cuando sea necesario
